d4b184c572506cdda8aa6593bd5b8dee5f48dccd
urluconeptual <ciocan_alexandra16@yahoo.com>
2024-01-08 11:53:12 +0200

    implemented player death logic

---
+++ Game/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    private Rigidbody2D _rb;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    private void Die() {
+        _rb.bodyType = RigidbodyType2D.Static;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.CompareTag("Enemy"));
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(1);
+            if(currentHealth == 0) {
+                Die();
+            }
+        }
+    }
+}

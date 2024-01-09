f363a686012dcd85bbf3070c0161f01285e18a59
Miruna Georgescu <mirubi12@gmail.com>
2024-01-09 15:42:07 +0200

    add tutorial

--- Game/Assets/Scripts/MainMenu.cs
+++ Game/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,11 @@ public class MainMenu : MonoBehaviour
    public void PlayGame() {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
-
+   
+   public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial"); 
+    }
    public void QuitGame() {
     Application.Quit();
    }

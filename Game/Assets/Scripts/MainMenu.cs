7ab9312fee9ef67e75cb6445242de4ed7bb679ba
urluconeptual <ciocan_alexandra16@yahoo.com>
2024-01-14 00:15:04 +0200

    save progress logic

--- Game/Assets/Scripts/MainMenu.cs
+++ Game/Assets/Scripts/MainMenu.cs
@@ -2,29 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+using System.Linq;
 
 public class MainMenu : MonoBehaviour
 {
-   public PlayerManager playerManager;
+    public PlayerManager playerManager;
+    public ProgressManager progressManager;
+    public TMP_InputField playerNameField;
+    public GameObject logInMenu;
+    public Button cancelSwitchPlayerButton;
+    public TMP_Text greetingsText;
 
-   public void PlayGame()
-   {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
+    void Start()
+    {
+        PlayersData playersData = playerManager.LoadFromJson();
+        bool logInMenuIsActive = playersData.currentPlayer == null;
+        logInMenu.SetActive(logInMenuIsActive);
+        if (logInMenuIsActive)
+        {
+            cancelSwitchPlayerButton.interactable = false;
+        }
+        else
+        {
+            DisplayGreetings(playersData.currentPlayer);
+        }
+    }
+    
+    void DisplayGreetings(string nameToDisplay)
+    {
+        greetingsText.text = "Welcome back, " + nameToDisplay + "!";
+    }
 
-   public void Tutorial()
-   {
-      SceneManager.LoadScene("Tutorial");
-   }
+    public void LogInPlayer()
+    {
+        PlayersData playersData = playerManager.LoadFromJson();
+        string inputName = playerNameField.text.ToString();
+        if (string.IsNullOrEmpty(inputName))
+        {
+            playerNameField.GetComponent<Image>().color = new Color(1f, 0.5f, 0.5f);
+            return;
+        }
+        if (!playersData.players.Any(inputName.Equals))
+        {
+            playersData.players.Add(inputName);
+        }
+        playersData.currentPlayer = inputName;
+        playerManager.SaveToJson(playersData);
+        logInMenu.SetActive(false);
+        cancelSwitchPlayerButton.interactable = true;
+        playerNameField.GetComponent<Image>().color = Color.white;
+        DisplayGreetings(inputName);
+    }
 
-   public void SwitchPlayer()
-   {
-      playerManager.SwitchPlayer();
-   }
+    private bool CheckNameIsValid(string inputName)
+    {
+        return !string.IsNullOrEmpty(inputName);
+    }
 
+    public void SwitchPlayer()
+    {
+        logInMenu.SetActive(true);
+    }
 
-   public void QuitGame()
-   {
-      Application.Quit();
-   }
+    public void CancelSwitchPlayer()
+    {
+        logInMenu.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        string lastCompletedLevel = progressManager.GetLastCompletedLevel();
+        if (lastCompletedLevel != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex  + (lastCompletedLevel[lastCompletedLevel.Length - 1] - '0') + 2);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
--- Game/Assets/Scripts/PlayerManager.cs
+++ Game/Assets/Scripts/PlayerManager.cs
@@ -3,69 +3,18 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
-    public TMP_InputField playerNameField;
-    public GameObject logInMenu;
-    public Button cancelSwitchPlayerButton;
     private string filePath = Application.dataPath + "/Data/PlayersDataFile.json";
-
-    public TMP_Text greetingsText;
-
-    void Start()
+    
+    public string CurrentPlayer
     {
-        PlayersData playersData = LoadFromJson();
-        bool logInMenuIsActive = playersData.currentPlayer == null;
-        logInMenu.SetActive(logInMenuIsActive);
-        if (logInMenuIsActive)
+        get
         {
-            cancelSwitchPlayerButton.interactable = false;
+            PlayersData playersData = LoadFromJson();
+            return playersData.currentPlayer;
         }
-        else
-        {
-            DisplayGreetings(playersData.currentPlayer);
-        }
-    }
-
-
-
-    public void LogInPlayer()
-    {
-        PlayersData playersData = LoadFromJson();
-        string inputName = playerNameField.text.ToString();
-        if (string.IsNullOrEmpty(inputName))
-        {
-            playerNameField.GetComponent<Image>().color = new Color(1f, 0.5f, 0.5f);
-            return;
-        }
-        if (!playersData.players.Any(inputName.Equals))
-        {
-            playersData.players.Add(inputName);
-        }
-        playersData.currentPlayer = inputName;
-        SaveToJson(playersData);
-        logInMenu.SetActive(false);
-        cancelSwitchPlayerButton.interactable = true;
-        playerNameField.GetComponent<Image>().color = Color.white;
-        DisplayGreetings(inputName);
-    }
-
-    private bool CheckNameIsValid(string inputName)
-    {
-        return !string.IsNullOrEmpty(inputName);
-    }
-
-    public void SwitchPlayer()
-    {
-        logInMenu.SetActive(true);
-    }
-
-    public void CancelSwitchPlayer()
-    {
-        logInMenu.SetActive(false);
     }
 
     public void SaveToJson(PlayersData playersData)
@@ -86,10 +35,5 @@ public class PlayerManager : MonoBehaviour
         {
             return new PlayersData();
         }
-    }
-
-    void DisplayGreetings(string nameToDisplay)
-    {
-        greetingsText.text = "Welcome back, " + nameToDisplay + "!";
     }
 }
